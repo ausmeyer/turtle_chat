71978c6608e8ac39e857ab5d51a0036182e67ae9
ausmeyer <austin.g.meyer@gmail.com>
2024-08-07 09:01:38 -0500

    adding requirements

--- turtle_chat_st.py
+++ turtle_chat_st.py
@@ -1,4 +1,5 @@
 import time
+import hmac
 import boto3
 import streamlit as st
 from botocore.exceptions import ClientError
@@ -6,6 +7,33 @@ from langchain.chains import ConversationChain
 from langchain_community.chat_models import BedrockChat
 from langchain.memory import ConversationBufferMemory
 import uuid
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
 st.title("Turtle Chat")
 
